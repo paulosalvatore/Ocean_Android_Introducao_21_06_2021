97f173731dd97ff3d8af44c252d90ee00be72858
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-21 12:43:34 -0300

    Referências de findViewById e mudança de texto do TextView

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_21_06_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_21_06_2021/MainActivity.kt
@@ -2,10 +2,19 @@ package com.oceanbrasil.ocean_android_introducao_21_06_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val etNome = findViewById<EditText>(R.id.etNome)
+
+        tvResultado.text = "Paulo Salvatore"
     }
 }
